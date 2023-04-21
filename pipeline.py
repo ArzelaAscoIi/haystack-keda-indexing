ebddae8b19d29c9fc89609868c9133ebe7dd4542
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-04-21 17:39:57 +0200

    docs: add example

--- pipeline.py
+++ pipeline.py
@@ -3,3 +3,8 @@ from haystack import Pipeline
 
 def get_pipeline(yaml_path: str) -> Pipeline:
     return Pipeline.load_from_yaml(yaml_path, pipeline_name="indexing")
+
+
+## example usage
+# pipeline = get_pipeline("./pipelines/pipeline.yaml")
+# documents = pipeline.run(file_paths=[Path(".your-file.txt")])
